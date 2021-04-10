3f0f8bc30b2159a23cf085a45e74526ecc98f014
Abhishek Muralikrishnan <54433137+AbhishekMuralikrishnan@users.noreply.github.com>
2021-04-09 18:15:21 -0700

    Frontend initial code

---
+++ frontend/scripts.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+
+    // Amazon momentum
+    var trace1 = {
+        x: [1, 2, 3, 4],
+        y: [10, 15, 13, 17],
+        type: 'scatter'
+    };
+    
+    var trace2 = {
+        x: [1, 2, 3, 4],
+        y: [16, 5, 11, 9],
+        type: 'scatter'
+    };
+    
+    var data = [trace1, trace2];
+
+
+    function populate_linechart(data, id){
+        Plotly.newPlot(id, data);
+    }
+
+    $("#amazon").on("click", function(){
+        $("#aapl").addClass('hide');
+        $("#ama").removeClass('hide');
+        $("#ama_active").addClass('active_bar');
+        $("#aapl_active").removeClass('active_bar');
+    });
+
+    $("#apple").on("click", function(){
+        $("#aapl").removeClass('hide');
+        $("#ama").addClass('hide');
+        $("#ama_active").removeClass('active_bar');
+        $("#aapl_active").addClass('active_bar');
+    });
+
+    populate_linechart(data, 'amazon_momentum');
+    populate_linechart(data, 'amazon_results');
+    populate_linechart(data, 'apple_momentum');
+    populate_linechart(data, 'apple_results');
+    $("#aapl").addClass('hide')
+});
