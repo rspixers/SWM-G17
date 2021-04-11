717f731c0e80805a4e743d3e193c1bbaa8c51f75
Prashanth <sramamo3@asu.edu>
2021-04-11 15:51:22 -0700

    flask boiler plate

---
+++ backend/api.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/api/add_message/<uuid>', methods=['GET', 'POST'])
+def add_message(uuid):
+    content = request.json
+    print(content['mytext'])
+    return jsonify({"uuid":uuid})
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0',debug=True)
