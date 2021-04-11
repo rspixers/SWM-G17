f11c7ab939ad564280fb825c1fe2009c5d1107cf
Prashanth <sramamo3@asu.edu>
2021-04-11 16:36:21 -0700

    Integration between front and backend

--- backend/api.py
+++ backend/api.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
+
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 @app.route('/api/add_message/<uuid>', methods=['GET', 'POST'])
+@cross_origin(supports_credentials=True)
 def add_message(uuid):
     content = request.json
     print(content['mytext'])
     return jsonify({"uuid":uuid})
 
-if __name__ == '__main__':
-    app.run(host= '0.0.0.0',debug=True)
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8000, debug=True)
